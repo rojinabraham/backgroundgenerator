ffa0bdbb28a7b3b5a093c88bdeafec505fe7ce81
rojinabraham <rojinabraham@gmail.com>
2020-04-22 15:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var _ = require("lodash");
-var array = [1, 2, 3, 4, 5];
-console.log("answer", _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
